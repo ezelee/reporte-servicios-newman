f4c4c659d1269657a1929e37efac339e869a9a78
ezelee <ezequiel.ariel.lee@gmail.com>
2021-11-10 20:42:21 -0300

    add node files

---
+++ newman/app.js
@@ -0,0 +1,40 @@
+const newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: './API.postman_collection.json',
+    reporters: ['htmlextra'],
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: '../index.html',
+            // template: './template.hbs'
+            // logs: true,
+            // showOnlyFails: true,
+            // noSyntaxHighlighting: true,
+            // testPaging: true,
+            browserTitle: "Reporte API",
+            title: "Reporte Servicios API",
+            // titleSize: 4,
+            // omitHeaders: true,
+            // skipHeaders: "Authorization",
+            // omitRequestBodies: true,
+            // omitResponseBodies: true,
+            // hideRequestBody: ["Login"],
+            // hideResponseBody: ["Auth Request"],
+            // showEnvironmentData: true,
+            // skipEnvironmentVars: ["API_KEY"],
+            // showGlobalData: true,
+            // skipGlobalVars: ["API_TOKEN"],
+            // skipSensitiveData: true,
+            // showMarkdownLinks: true,
+            // showFolderDescription: true,
+            timezone: "America/Argentina/Buenos_Aires",
+            // skipFolders: "folder name with space,folderWithoutSpace",
+            // skipRequests: "request name with space,requestNameWithoutSpace"
+        }
+    }
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
