db92fbf32fccd67f3c4cb983199efae474c9d6f9
ezelee <elee@sancorseguros.com>
2021-11-19 09:15:33 -0300

    windows task scheduler commit

--- newman/app.js
+++ newman/app.js
@@ -1,8 +1,18 @@
+require('dotenv').config({path: __dirname + '/.env'})
+//const apiToken = process.env['API_TOKEN'];
+const apiToken = '';
 const newman = require('newman'); // require newman in your project
+
+var postmanCollection = '';
+if (apiToken) {
+	postmanCollection = 'https://api.getpostman.com/collections/10402387-5398570a-ba98-4a4d-97f9-d5695e4d85e2?apikey=' + apiToken
+} else {
+	postmanCollection = './newman/API.postman_collection.json'
+}
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: './newman/API.postman_collection.json',
+    collection: postmanCollection,
     reporters: ['htmlextra'],
     iterationCount: 1,
     reporter: {
